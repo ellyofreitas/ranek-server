6b80a7b12f906d65d4f082a27a3cc6afa7c4e6ef
Ellyo Freitas <ellyofreitas13@gmail.com>
2020-01-19 01:21:12 -0300

    feat: implements auth session route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 
 import AuthMiddleware from './app/middlewares/auth';
@@ -11,16 +12,21 @@ const routes = Router();
 
 const upload = multer(multerConfig);
 
-routes.get('/users', UserController.index);
+// Routes unauthenticated
+
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
+
+routes.use(AuthMiddleware); // Middleware
+
+// Routes authenticated
+
+routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
 routes.put('/users/:id', UserController.edit);
 routes.delete('/users/:id', UserController.destroy);
 
 routes.post('/files', upload.array('file'), FileController.store);
-
-routes.use(AuthMiddleware);
-
-// Routes authenticated
 
 export default routes;
