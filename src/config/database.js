e5d1402ed08627fd0c70a02e3c3a59f29ee7cf54
Ellyo Freitas <ellyofreitas13@gmail.com>
2020-03-15 16:09:37 -0300

    fix: sequelize logs in production

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  logging: () => process.env.NODE_ENV === 'development' && true,
   define: {
     timestamps: true,
     underscored: true,
